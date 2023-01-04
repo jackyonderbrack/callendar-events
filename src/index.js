abf896ccd5ab1faa2d36c9d3bd2ba8e88d7b60dc
jackyonderbrack <mike.projektowanie@gmail.com>
2023-01-04 10:53:13 +0100

    Added SessionContextProvider from @supabase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+
+const supabase = createClient(
+  "https://ipxxgrgagfvttzjshijr.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlweHhncmdhZ2Z2dHR6anNoaWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI4MjQ1NDAsImV4cCI6MTk4ODQwMDU0MH0.NscIwewSEstUbrybEAfdN3FpWq7BkqoMLvRM0C3Z4AA"
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    {/* SessionContextProvider -> Now our application gets access to supabase */}
+    <SessionContextProvider supabaseClient={supabase}>
+
+      <App />
+      
+    </SessionContextProvider>
+    
   </React.StrictMode>
 );
 
